02ef1553858f4485a27ac6b3b51de1d9757a0039
Christian Kimball <christian.kimball@cgi.com>
2018-08-06 16:13:44 -0400

    Publish to GitHub

---
+++ appui/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { ratingContract } from "./setup";
+import {ShowMovies } from "./ShowMovies";
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      movies : [{name:'Star Wars',rating: ratingContract.totalVotesFor('Star Wars').toNumber()},{name:'Avatar',rating: ratingContract.totalVotesFor('Avatar').toNumber()},{name:'Inception',rating: ratingContract.totalVotesFor('Inception').toNumber()}]
+    }
+    this.handleVoting=this.handleVoting.bind(this)
+  }
+
+  handleVoting(movie){
+    ratingContract.voteForMovie(movie)
+    let votes=ratingContract.totalVotesFor(movie).toNumber()
+    this.setState({movies:this.state.movies.map(
+      (el)=>el.name===movie? Object.assign({},el,{rating:votes}):el
+    
+    )});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Ethereum</h1>
+        </header>
+        <p className="App-intro">
+          Movie Rating Application in Ethereum and React
+        </p>
+        <div className="movie-table">
+          <ShowMovies movies={this.state.movies} vote={this.handleVoting}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
